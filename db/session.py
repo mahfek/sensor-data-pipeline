3e7685429cc3e71066ff7c7300e20b4a467f8524
mahfek <45622248+mahfek@users.noreply.github.com>
2025-10-05 15:02:12 +0200

    Update session.py

--- db/session.py
+++ db/session.py
@@ -8,7 +8,7 @@ engine = create_engine(
     settings.sqlalchemy_uri,
     pool_size=settings.DB_POOL_SIZE,
     max_overflow=settings.DB_MAX_OVERFLOW,
-    pool_pre_ping=True,  # جلوگیری از dead connection
+    pool_pre_ping=True,  #  dead connection
 )
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
